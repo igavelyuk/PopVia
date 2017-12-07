737078c579bb7a2abb2291f974b21dddd136b59d
EvilEpicCoder <1st.elixile@gmail.com>
2017-12-07 23:59:30 +0200

    data

--- Cripto/pay.js
+++ Cripto/pay.js
@@ -1,20 +1,48 @@
-/************************************
-* Project to calculate average time *
-* read of the article.              *
-* EEC_timeOfRead by EEC             *
-************************************/
+/*****************************************
+* Project to work with https://gourl.io/ *
+* Pay by EEC for popvia.com              *
+*****************************************/
 var Crypto = {
+  status: 'payment_not_received',
+  err: '',
+  public_key: '20AAvZCcgBitcoin77BTCPUB0xyyeKkxMUmeTJRWj7IZrbJ0oL',
+  box: '20',
+  boxtype: 'paymentbox',
+  order: 'invoice22',
+  user: '102671958__9ffd317053',
+  userformat: 'COOKIE',
+  usercountry: '',
+  period: '',
+  amount: '0.00023998',
+  amountusd: '3.11',
+  wallet_url: 'bitcoin:1FvJPe8un1qHRYePGYQPWHZTwqQhC2zo4h?amount=0.00023998&amp;label=Payment',
+  coinlabel: 'BTC',
+  coinname: 'Bitcoin',
+  addr: '1FvJPe8un1qHRYePGYQPWHZTwqQhC2zo4h',
+  tx: "",
+  tx_url: "",
+  confirmed: "0",
+  timestamp: "",
+  date: "",
+  datetime: "",
+  json: "1",// currency data section
+  ip_addr:'',
+  code:'',
+  country:'',
+  city:'',
+  posX:'',
+  posY:'',
   initCrypto: ()=>{
-    console.log("function initCrypto()");
+    // console.log("function initCrypto()");
   },
   sendPayment:()=>{
-    console.log("function sendPayment()");
+    // console.log("function sendPayment()");
   }
 };
 window.onload=function(){
   console.log('Crypto start');
   var paymentObject=new Object(Crypto);
-  paymentObject.initCrypto;
+  paymentObject.initCrypto();
 
   const button=document.getElementById("sendPayment");
   button.addEventListener("click",paymentObject.sendPayment,false);
