1ae3029b52bbe981c215bb1fec96183fd30b9162
EvilEpicCoder <1st.elixile@gmail.com>
2017-12-07 21:56:18 +0200

    base js class

--- Cripto/pay.js
+++ Cripto/pay.js
@@ -1,1 +1,17 @@
-// Noting yet
+/************************************
+* Project to calculate average time *
+* read of the article.              *
+* EEC_timeOfRead by EEC             *
+************************************/
+var Crypto = {
+  initCrypto()=>{
+    console.log("function initCrypto()");
+  }
+};
+window.onload=function(){
+  console.log('Crypto start');
+  var paymentObject=new Object(Crypto);
+  paymentObject.initCrypto();
+  const button=document.getElementById("sendPayment");
+  button.addEventListener("click",a.initTFR,false);
+}
