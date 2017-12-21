3921389ecde0b87f0fc3371ff0039feabd0142dd
EvilEpicCoder <1st.elixile@gmail.com>
2017-12-21 02:35:16 +0200

    Security Update

--- Crypto/static/pay.js
+++ Crypto/static/pay.js
@@ -39,7 +39,7 @@ var SecurityCheck = () => {
   // console.log(firstCheck);
   // console.log(secondCheck);
   // console.log(thirdCheck.length+"---------");
-  if(firstCheck>10&&thirdCheck.length==64){
+  if(firstCheck>10&&thirdCheck.length==64){ // == 64 test
     $('#sendPayment').removeClass("disabled");
   }else{
     $('#sendPayment').addClass('disabled');
